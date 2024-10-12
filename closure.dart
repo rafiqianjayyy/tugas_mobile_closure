b0e384be6c3e7f7cf7df3eb42a39bd7e05e9e032
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-12 22:48:17 +0700

    Create closure.dart

---
+++ closure.dart
@@ -0,0 +1,12 @@
+//INI ADALAH CONTOH PENGGUNAAN CLOSURE
+// void main(){
+//   var bilangan = 0;
+//   void increment(){
+//     print('increment');
+//     bilangan++;
+//   }
+
+//   increment();
+//   increment();
+//   print(bilangan);
+// }
